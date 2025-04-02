94cc2ed33094249c9fd9ee62669ef66fa1adca68
APPEX Bolivia <leonardo.eguez@appexbo.com>
2025-04-02 12:37:39 -0400

    update

--- l10n_py_operations/models/account_move_send.py
+++ l10n_py_operations/models/account_move_send.py
@@ -2,14 +2,14 @@
 
 from odoo import api, models, fields
 import base64
-
+import io
 import logging
 _logger = logging.getLogger(__name__)
 
 
 
 class AccountMoveSend(models.TransientModel):
-    _inherit = ['account.move.send']    
+    _inherit = ['account.move.send']   
 
     
     
@@ -18,17 +18,18 @@ class AccountMoveSend(models.TransientModel):
         if invoice.invoice_pdf_report_id:
             return
 
-        #content, _report_format = self.env['ir.actions.report']._render(f'l10n_bo_bolivian_invoice.ir_actions_report_invoice_bo_{invoice[0].pos_id.paper_format_type}', invoice.ids)
+       
         if invoice[0].company_id.use_auto_invoice:
-            content, _report_format = self.env['ir.actions.report']._render(f'l10n_py_operations.ir_actions_report_invoice_py_1', invoice.ids)
-
+            content, _report_format = self.env['ir.actions.report']._render(
+                 'l10n_py_operations.ir_actions_report_invoice_py_1', invoice.ids
+             )
             invoice_data['pdf_attachment_values'] = {
                 'raw': content,
                 'name': invoice._get_invoice_report_filename(),
                 'mimetype': 'application/pdf',
                 'res_model': invoice._name,
                 'res_id': invoice.id,
-                'res_field': 'invoice_pdf_report_file', # Binary field
+                'res_field': 'invoice_pdf_report_file',  # Binary field
             }
         else:
             content = invoice.get_request_pdf()
@@ -48,4 +49,3 @@ class AccountMoveSend(models.TransientModel):
                 }
             else:
                 super(AccountMoveSend, self)._prepare_invoice_pdf_report(invoice, invoice_data)
-    
