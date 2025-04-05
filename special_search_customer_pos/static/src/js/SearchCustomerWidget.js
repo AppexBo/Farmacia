08f99cf661b37daad03e1b120491104600185385
Erick Denis <erickmercadooudalova@gmail.com>
2025-04-05 21:41:27 +0300

    test get log info

--- special_search_customer_pos/static/src/js/SearchCustomerWidget.js
+++ special_search_customer_pos/static/src/js/SearchCustomerWidget.js
@@ -1,11 +1,44 @@
-/** @odoo-module */
-import { Order } from "@point_of_sale/app/store/models";
-import { patch } from "@web/core/utils/patch";
+odoo.define(
+    '@pos_loyalty/overrides/components/partner_list_screen/partner_list_screen', 
+    [
+        '@point_of_sale/app/screens/partner_list/partner_list',
+        '@web/core/utils/patch'
+    ], 
+    function(require) {
+        "use strict";
 
-patch(Order.prototype, {
-    setup() {
-        super.setup(...arguments);
-		//Cambios en el POS
-        this.changePos(this.pos);
-	}, 
-});
+        const { PartnerListScreen } = require("@point_of_sale/app/screens/partner_list/partner_list");
+        const { patch } = require("@web/core/utils/patch");
+
+        patch(PartnerListScreen.prototype, {
+            get partners() {
+                console.log("[DEBUG] partners - query:", this.state.query || "(vacía)"); // Log añadido
+                
+                let res;
+                if (this.state.query && this.state.query.trim() !== "") {
+                    res = this.pos.db.search_partner(this.state.query.trim());
+                } else {
+                    res = this.pos.db.get_partners_sorted(1000);
+                }
+
+                res.sort(function(a, b) {
+                    return (a.name || "").localeCompare(b.name || "");
+                });
+
+                if (this.state.selectedPartner) {
+                    const indexOfSelectedPartner = res.findIndex(
+                        (partner) => partner.id === this.state.selectedPartner.id
+                    );
+                    if (indexOfSelectedPartner !== -1) {
+                        res.splice(indexOfSelectedPartner, 1);
+                    }
+                    res.unshift(this.state.selectedPartner);
+                }
+
+                return res;
+            }
+        });
+
+        return {};
+    }
+);
