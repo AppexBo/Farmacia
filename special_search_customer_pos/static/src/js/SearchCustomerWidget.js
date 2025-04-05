162be278ecabcf37d473a8d592a6775c4822fef5
Erick Denis <erickmercadooudalova@gmail.com>
2025-04-05 21:15:36 +0300

    test get log info

--- special_search_customer_pos/static/src/js/SearchCustomerWidget.js
+++ special_search_customer_pos/static/src/js/SearchCustomerWidget.js
@@ -1,6 +1,11 @@
-//import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list_screen";
+/** @odoo-module */
+import { Order } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 
-patch(
-
-);
+patch(Order.prototype, {
+    setup() {
+        super.setup(...arguments);
+		//Cambios en el POS
+        this.changePos(this.pos);
+	}, 
+});
