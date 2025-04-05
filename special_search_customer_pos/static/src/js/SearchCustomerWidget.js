5ac3e0e8f1274693bac74e7a9e805a51b8d9f677
Erick Denis <erickmercadooudalova@gmail.com>
2025-04-05 20:28:15 +0300

    test get log info

--- special_search_customer_pos/static/src/js/SearchCustomerWidget.js
+++ special_search_customer_pos/static/src/js/SearchCustomerWidget.js
@@ -1,5 +1,4 @@
 import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list_screen";
-const {PartnerLine} = require("@point_of_sale/app/screens/partner_list/partner_line/partner_line");
 import { patch } from "@web/core/utils/patch";
 import { Component, onMounted, useRef } from "@odoo/owl";
 
