8cc7be76a60f73d97f27f4fe5adffddab14c00ce
Erick Denis <erickmercadooudalova@gmail.com>
2025-04-05 21:08:48 +0300

    test get log info

--- special_search_customer_pos/static/src/js/SearchCustomerWidget.js
+++ special_search_customer_pos/static/src/js/SearchCustomerWidget.js
@@ -1,13 +1,6 @@
-import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list_screen";
-
-console.log("asd");
-/*import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list_screen";
-
-
+//import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list_screen";
 import { patch } from "@web/core/utils/patch";
 
-console.log("asd");
+patch(
 
-patch(PartnerListScreen.prototype, { 
-    
-});*/
+);
